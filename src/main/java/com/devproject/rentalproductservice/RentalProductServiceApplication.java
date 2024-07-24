88fb4ed74cb68714180a9a0650163e91d14d6f53
bhaskardixit <bhaskar.dixit@xebia.com>
2024-07-24 20:24:04 +0530

    init proj wt security

---
+++ src/main/java/com/devproject/rentalproductservice/RentalProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devproject.rentalproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalProductServiceApplication.class, args);
+	}
+
+}
